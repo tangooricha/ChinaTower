23715209a8f9d44f4a69b1fb2964201cb0522ec8
あまみや ゆうこ <1@1234.sh>
2015-11-29 16:35:30 +0800

    RxLev informations output

--- src/ChinaTower.StationPlanning/Controllers/TowerController.cs
+++ src/ChinaTower.StationPlanning/Controllers/TowerController.cs
@@ -25,5 +25,38 @@ namespace ChinaTower.StationPlanning.Controllers
                 towers = towers.Where(x => x.Provider == provider.Value);
             return AjaxPagedView(towers, "lst-towers");
         }
+
+        public IActionResult RxLev(double? left, double? right, double? top, double? bottom, int? year, int? month)
+        {
+            IEnumerable<RxLevLine> signals = DB.RxLevLines;
+            if (right.HasValue && right - left > 1.188078574995771)
+            {
+                return Json(new List<RxLevLine>());
+            }
+            else
+            {
+                if (year.HasValue)
+                    signals = signals.Where(x => x.Time.Year == year.Value);
+                if (month.HasValue)
+                    signals = signals.Where(x => x.Time.Month == month.Value);
+                if (left.HasValue)
+                    signals = signals.Where(x => x.BeginLon >= left.Value || x.EndLon >= left.Value);
+                if (right.HasValue)
+                    signals = signals.Where(x => x.BeginLon <= right.Value || x.EndLon <= right.Value);
+                if (top.HasValue)
+                    signals = signals.Where(x => x.BeginLat <= top.Value || x.EndLat <= top.Value);
+                if (bottom.HasValue)
+                    signals = signals.Where(x => x.BeginLat >= bottom.Value || x.EndLat >= bottom.Value);
+                if (User.IsInRole("Member"))
+                {
+                    var claims = User.Claims
+                        .Where(x => x.Type == "有权限访问地市数据")
+                        .Select(x => x.Value)
+                        .ToList();
+                    signals = signals.Where(x => claims.Contains(x.City));
+                }
+                return Json(signals.ToList());
+            }
+        }
     }
 }
